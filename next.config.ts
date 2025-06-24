1e1eb9e4ce7c77f08a915ab4900dbb7b3d80b766
Alexis Reyna <alexis.rs@inverater.com>
2025-06-23 23:51:01 -0600

    alias

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@assets': path.resolve(__dirname, 'assets'),
+    };
+    return config;
   },
 };
 
